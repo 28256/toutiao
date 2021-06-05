5e53dd4395415e12d669c99318440d72d9d540d6
wangchao <wangchao602@126.com>
2021-06-05 15:32:48 +0800

    authenction  提交

---
+++ src/main/java/com/nowcoder/shiro/MyRealm.java
@@ -0,0 +1,103 @@
+package com.nowcoder.shiro;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.realm.SimpleAccountRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class MyRealm extends AuthorizingRealm {
+
+    /**
+     * 模拟数据库
+     * @param principals
+     * @return
+     */
+    Map<String,String> userMap = new HashMap<>(1);
+    {
+        userMap.put("root","12345");
+        super.setName("myRealm");
+    }
+
+    /**
+     * 授权
+     * @param principals
+     * @return
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        String userName = String.valueOf(principals.getPrimaryPrincipal());
+
+        // 从数据库获取角色和权限数据
+        Set<String> permissionSet = getPermessionGetByUserName(userName);
+        Set<String> rolesSet = getRolesGetByUserName(userName);
+
+        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+        simpleAuthorizationInfo.setStringPermissions(permissionSet);
+        simpleAuthorizationInfo.setRoles(rolesSet);
+        return simpleAuthorizationInfo;
+    }
+
+    /**
+     * 认证
+     * @param token
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+
+        // 1.从主体传过来的认证信息中，获得用户名
+        String userName = String.valueOf(token.getPrincipal());
+
+        // 2.通过用户名到数据库中获取凭证
+        String passWord = getPassWordByUserName(userName);
+        if(StringUtils.isBlank(passWord)){
+            return null;
+        }
+
+        SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(userName,passWord,"myRealm");
+        return simpleAuthenticationInfo;
+    }
+
+    /**
+     * 模拟从数据库中获取权限
+     * @param
+     * @return
+     */
+    private Set<String> getPermessionGetByUserName(String userName){
+        Set<String> permission = new HashSet<>();
+        permission.add("can update");
+        permission.add("can delete");
+        return permission;
+    }
+
+    /**
+     * 模拟从数据库中获取角色
+     * @param
+     * @return
+     */
+    private Set<String> getRolesGetByUserName(String userName){
+        Set<String> roles = new HashSet<>();
+        roles.add("admain");
+        roles.add("user");
+        return roles;
+    }
+
+    /**
+     * 数据库获取凭证
+     */
+    private String getPassWordByUserName(String userName){
+        return userMap.get(userName);
+    }
+}
